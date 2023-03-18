e91f88bf6ebe092283ce94c86443288fa40be4d0
Enoidd <enoid994@gmail.com>
2023-03-18 13:19:32 +0100

    *****

---
+++ Overloading/src/overloarding/Overloading.java
@@ -0,0 +1,44 @@
+package overloarding;
+
+public class Overloading {
+	
+	/* Metodi */
+	/* METODO SOVRACCARICO --->>> Overloading: 
+	 * Permette di definire molteplici metodi / procedure con lo stesso nome.
+	 * Chiamate anche "versioni sovraccariche", queste devono risultare distinguibili.
+	 * 
+	 * In che modo devono distinguersi?
+	 * Numero di parametri
+	 * Almeno un tipo tra i parametri deve essere diverso
+	 */
+	
+	public void metodo(int a) {
+		System.out.println("parametro int");
+	}
+	
+	public void metodo(double a) {
+		System.out.println("parametro double");
+	}
+	
+	/* Le versioni sovraccariche dello stesso metodo possono differire anche per il
+	 ")* tipo di valore restituito
+	 */
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public double add(int a, double b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Overloading prova = new Overloading();
+		prova.metodo(3);	// stampa "parametro int"
+		prova.metodo(3.0d);	// stampa "parametro double"
+		
+		
+		
+	}
+
+}
